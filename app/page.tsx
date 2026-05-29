33c936102e27c9e79da7e0ba8965dd5f94148f3d
Cryptotenor <97893837+Cryptotenor@users.noreply.github.com>
2026-05-29 23:34:43 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "white",
+        color: "black",
+        fontFamily: "Helvetica, Arial, sans-serif",
+        gap: "1rem",
+      }}
+    >
+      <h1>Yi-An Chen</h1>
+
+      <p>
+        hello
+      </p>
+    </main>
+  );
+}
